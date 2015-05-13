945f68d08e610c7fcb22e83ca0a12031c8b8edef
saulosantiago <saulodasilvasantiago@gmail.com>
2015-05-13 03:13:52 -0300

    build css basic

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,2 +1,6 @@
 //= require jquery-2.1.4
 //= require materialize
+
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+});
--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -3,4 +3,3 @@ require 'sprockets'
 
 Sprockets.append_path "/_vendor/stylesheets/"
 Sprockets.append_path "/_vendor/javascripts/"
-Sprockets.append_path "/_vendor/fonts/"
