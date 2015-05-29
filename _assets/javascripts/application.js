20423779e400ae8049d6399e2b10de659515d240
Saulo da Silva <saulo@Saulos-MacBook-Pro-2.local>
2015-05-29 01:03:56 -0300

    adding lazy load for images

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,6 +1,10 @@
 //= require jquery-2.1.4
+//= require jquery.lazyload
 //= require materialize
 
 $(document).ready(function() {
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav();
+  $('img.lazy').lazyload({
+    effect : 'fadeIn'
+  });
 });
